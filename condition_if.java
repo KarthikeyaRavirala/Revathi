269ef70a02e7846ee36c5bc723088e47f423121a
karthikeya ravirala <132122712+KarthikeyaRavirala@users.noreply.github.com>
2025-05-23 13:20:23 +0530

    Create condition_if.java

---
+++ condition_if.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+public static void main(String []args){
+  if(System.out.printf("Ice")!=null)
+    System.out.printf("Cream");
+  else
+    System.out.printf("out of scope");
+  }
+}
