4de5748bce3e6ff8c95f09178ecf44fca81e9b77
karthikeya ravirala <132122712+KarthikeyaRavirala@users.noreply.github.com>
2025-05-23 13:30:56 +0530

    Update condition_if.java

--- condition_if.java
+++ condition_if.java
@@ -7,3 +7,11 @@ public static void main(String []args){
     System.out.printf("out of scope");
   }
 }
+
+if(System.out.printf("")!=null)
+// if the "Ice" is not present the code will return "Cream"
+// such that the if statement will become null 
+  
+PrintStream ps=null
+if(ps!=null)
+// The else part will work
